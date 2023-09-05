80c8a38cd11e0380c8549f8e6855c0aad1318f53
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-05 19:52:51 +0100

    Create User model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
+  username: { type: String, required: true, unique: true, index: true },
+  password: { type: String, required: true },
+  is_member: { type: Boolean, required: true, default: false },
+});
+
+UserSchema.virtual('url').get(function () {
+  return `/user/${this._id}`;
+});
+
+UserSchema.virtual('full_name').get(function () {
+  return `${this.name} ${this.surname}`;
+});
+
+module.exports = mongoose.model('User', UserSchema);
