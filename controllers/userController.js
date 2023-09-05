3b305bebf43f29cfcf55fc9a24b8ea0177b66809
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-05 20:26:57 +0100

    Create user_create_post (no-hashing)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,48 @@
 const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
-const { body, resultValidator } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 exports.user_create_get = (req, res, next) => {
   res.render('user_sign_up', { title: 'Sign Up' });
 };
+
+exports.user_create_post = [
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Name cannot be empty.'),
+  body('surname')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Surname cannot be empty.'),
+  body('surname')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Username cannot be empty.'),
+
+  asyncHandler(async (req, req, next) => {
+    const errors = validationResult(req);
+
+    const user = new User({
+      name: req.body.name,
+      surname: req.body.surname,
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('user_signup', {
+        title: 'Sign Up',
+        user: user,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await user.save();
+      res.redirect(user.url);
+    }
+  }),
+];
