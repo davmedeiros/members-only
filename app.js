7261e10f1c847f591a1362e18a32d3162c882576
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-06 19:23:32 +0100

    Show welcome message to loged on users

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/sign-up', signUpRouter);
 app.use('/login', loginRouter);
