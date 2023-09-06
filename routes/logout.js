5eca4d23b8820f40bf2c014d02793d1f3dc8559d
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-06 19:31:15 +0100

    Change function to arrow

--- routes/logout.js
+++ routes/logout.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  req.logout(function (err) {
+  req.logout((err) => {
     if (err) {
       return next(err);
     }
